59e756eea21a519b9d0255170ba27b2822506f87
codewithbingle <codewithbingle@gmail.com>
2023-04-18 19:53:25 -0700

    string-alert-if-it-is-a-question

---
+++ 8kyu/string-alert-if-it-is-a-question.js
@@ -0,0 +1,5 @@
+// Declare a variable and assign it to a sentance as a string. Alert if the sentance is a question
+
+// Solution
+const str = "Is this the best week ever?";
+alert(str.endsWith("?"));
